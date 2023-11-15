14b02229f9871779ed2c1b9570108b96c38dad23
MuriLovin <silvamurilo689@gmail.com>
2023-11-15 19:01:08 -0300

    refactor: removing comments

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,11 +6,4 @@ export default registerAs('database', () => ({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  //   synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
-  //   logging: process.env.DATABASE_LOGGING === 'true',
-  //   entities: ['dist/**/*.entity.js'],
-  //   migrations: ['dist/migrations/*.js'],
-  //   cli: {
-  //     migrationsDir: 'src/migrations',
-  //   },
 }));
